e21cc7e79a254da521ffe2942f79d2424968eae5
Sergey Bokhantsev <sergey.bokhantsev@invisiblecrm.com>
2020-03-31 22:12:27 +0300

    init

---
+++ app/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace myapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = 0;
+            var max = 5;
+            while (max == -1 || counter < max)
+            {
+                counter++;
+                Console.WriteLine($"Counter: {counter}");
+                System.Threading.Tasks.Task.Delay(1000).Wait();
+            }
+        }
+    }
+}
